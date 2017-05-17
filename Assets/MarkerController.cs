6a17bebc9e001131739a77ecb45248b6ac4d0a5b
fatboyspy <fatboy_spy@mail.ru>
2017-05-17 08:22:29 +0300

    First commit
    First commit all project

---
+++ Assets/MarkerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerController : MonoBehaviour {
+    [SerializeField]
+    GameObject marker;
+    [SerializeField]
+    Animator animator;
+    string previousName;
+	// Use this for initialization
+	void Start () {
+        marker.SetActive(false);
+        animator = marker.GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    public void PlayAnimation()
+    {
+        AnimatorStateInfo asi = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (asi.IsName("Idle"))
+        {
+            if (previousName == null || previousName == "interactive_02")
+            {
+                previousName = "interactive_01";
+                animator.PlayInFixedTime(previousName);
+            }
+            else
+            {
+                previousName = "interactive_02";
+                animator.PlayInFixedTime(previousName);
+            }
+        }
+        
+    }
+}
